21f88004bcf21dc4c870f016331458f387b23ac9
docente.dev <docente.dev@gmail.com>
2023-10-02 00:10:35 -0300

    feature: v1.0.2 add usersWhoSupport

--- src/app/Card.tsx
+++ src/app/Card.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { roboto } from './page';
 
 function Card() {
     return (
@@ -11,7 +12,7 @@ function Card() {
                     allowFullScreen />
             </div>
 
-            <div className="card-body">
+            <div className={`card-body ${roboto.className}`}>
                 <h5 className="card-title">Discurso del Presidente Boric durante la histórica convocatoria ciudadana del 30/09</h5>
                 <p className="card-text">Discurso del Presidente Boric durante la histórica convocatoria ciudadana del 30/09, en la Plaza de la Constitución. Agradecimientos a todos los que asistieron a los distintos puntos de encuentro a nivel nacional, vamos ganando experiencia, esto recién comienza #BoricEstamosContigo #Boricnoestasolo</p>
             </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,71 @@
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 import { Roboto, Oswald } from 'next/font/google'
-import { TwitterTweetEmbed } from 'react-twitter-embed';
-
-const roboto = Roboto({
-  weight: '400',
-  subsets: ['latin'],
-})
-
-const oswald = Oswald({
-  weight: '400',
-  subsets: ['latin'],
-})
+import Link from 'next/link';
 
 import styles from './page.module.css'
 import mainImage from './images/main.png'
 import Card from './Card';
 
+import tiktok_AtJusticieroAnonimo from './users/tiktok_@justiciero.anonimo.jpeg';
+import tiktok_AtClaudiodeviaje from './users/tiktok_@claudiodeviaje.jpeg';
+import tiktok_AtLoretoAPC from './users/tiktok_@loreto.a.p.c.jpeg';
+import tiktok_AtMarianiits_ from './users/tiktok_@marianiits_.jpeg';
+import tiktok_AtZurda38porciento from './users/tiktok_@zurda38porciento.jpeg';
+import tiktok_AtManque1978 from './users/tiktok_@manque1978.jpeg';
+
+
+import Footer from './Footer';
+
+export const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
+
+export const oswald = Oswald({
+  weight: '400',
+  subsets: ['latin'],
+})
+
+type SupportingUser = {
+  username: string,
+  image: StaticImageData,
+  link: string,
+}
+
+const usersWhoSupport: SupportingUser[] = [{
+  username: '@justiciero.anonimo',
+  image: tiktok_AtJusticieroAnonimo,
+  link: 'https://www.tiktok.com/@justiciero.anonimo/video/7284627625425849606',
+}, {
+  username: '@claudiodeviaje',
+  image: tiktok_AtClaudiodeviaje,
+  link: 'https://www.tiktok.com/@claudiodeviaje/video/7284630001217998086',
+}, {
+  username: '@loreto.a.p.c',
+  image: tiktok_AtLoretoAPC,
+  link: 'https://www.tiktok.com/@loreto.a.p.c/video/7285087429239786758?lang=es',
+}, {
+  username: '@marianiits_',
+  image: tiktok_AtMarianiits_,
+  link: 'https://www.tiktok.com/@marianiits_/video/7284669192681311494?lang=es',
+}, {
+  username: '@zurda38porciento',
+  image: tiktok_AtZurda38porciento,
+  link: 'https://www.tiktok.com/@zurda38porciento/video/7283913328940862725?lang=es',
+}, {
+  username: '@manque1978',
+  image: tiktok_AtManque1978,
+  link: 'https://www.tiktok.com/@manque1978/video/7283198729128054021?lang=es',
+}];
+
 export default function Home() {
   return (
-    <main className={`${styles.main} ${roboto.className} ${oswald.className}`}>
+    <main className={`${styles.main}`}>
       <header>
         <div>
           <div>
-            <h1>Boric estamos contigo</h1>
-            <h2>Presidente 2022 - 2026</h2>
+            <h1 className={`${oswald.className}`}>Boric estamos contigo</h1>
+            <h2 className={`${oswald.className}`}>Presidente 2022 - 2026</h2>
           </div>
           <div>
             <Image
@@ -38,10 +80,26 @@ export default function Home() {
         </div>
       </header>
       <section>
-        <article>
-        </article>
+        <Card />
       </section>
-      <Card />
+      <section>
+        <h2 className={`text-center ${oswald.className} mt-4`}>Nosotros apoyamos al presidente</h2>
+        <div className="row">
+          <div className="col">
+            <div className={styles.supportingUserContainer}>
+              {usersWhoSupport.map((e: SupportingUser, i: number) => (
+                <Link href={e.link} target='_blank' key={i}>
+                  <article className={styles.supportingUser}>
+                    <Image src={e.image} width={80} height={80} alt={e.username} />
+                    <h3>{e.username}</h3>
+                  </article>
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer/>
     </main>
   )
 
